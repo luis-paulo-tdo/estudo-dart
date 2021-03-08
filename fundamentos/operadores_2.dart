edf5d6d0141ea006de8e99cb2853530df5e40115
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-08 07:46:32 -0300

    Demonstração do uso dos Operadores de Atribuição, Relacionais e Lógicos Bit a Bit. Os Operadores de Atribuição tem como objetivo inputar valores em variáveis, são Binários e Infix, sendo eles o Igual (=); Mais Igual (+=); Menos Igual (-=); Vezes Igual (*=); Dividido Igual (/=) e Módulo Igual (%=). Com exceção do Igual, os demais Operadores de Atribuição aplicam uma operação aritmética entre o valor da variável e o que é recebido por ela. Os Operadores Relacionais visam comparar valores, são Binários e Infix, sendo eles o Maior que (>); Menor que (<); Maior ou igual a (>=); Menor ou igual a (<=); Igual a (==) e Diferente de (!=). Para os Operadores Relacionais de Igualdade (== e !=), o mesmo valor com tipo diferente (3 e '3') são diferentes. Já os Operadores Lógicos Bit a Bit convertem os operandos em Bits e aplicam a operação lógica sobre eles, são Binários e Infix, sendo eles o E Bit a Bit (&); OU Bit a Bit (|) e XOU Bit a Bit (^). Utilizando os Operadores Aritméticos, Lógicos e Relacionais em conjunto, é possível criar Expressões mais complexas.

---
+++ fundamentos/operadores_2.dart
@@ -0,0 +1,27 @@
+main() {
+  // Operadores de Atribução
+  double numberOne = 2;
+  numberOne += 3;
+  numberOne -= 3;
+  numberOne *= 3;
+  numberOne /= 3;
+  numberOne %= 3;
+  print(numberOne);
+
+  // Operadores Relacionais
+  print(3 > 2);
+  print(3 < 2);
+  print(3 >= 2);
+  print(3 <= 2);
+  print(3 != 2);
+  print(3 == 2);
+  print(3 == '3');
+
+  // Expressões com Operadores Aritméticos, Lógicos e Relacionais
+  print(3 + 5 > 3 - 1 && 4 + 7 != 7 - 4);
+
+  // Operadores Lógicos Bit a Bit
+  print(5 & 4);
+  print(5 | 4);
+  print(5 ^ 4);
+}
