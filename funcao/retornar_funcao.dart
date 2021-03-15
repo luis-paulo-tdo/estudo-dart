09262626ef7da4090678c582f53060f0fef1fa2e
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-15 08:19:01 -0300

    Demonstração do uso de Funções que retornam outras Funções como uma técnica de melhoria de performance. Em cenários onde uma Função que utilizam diferentes Algoritmos para trabalhar com diferentes Parâmetros, podem surgir demandas de processamento desnecessárias caso estes Parâmetros possuem valores iguais quando chamados diversas vezes. Caso esta mesma Função execute apenas a parte do Algoritmo que trabalhe com mais frequência sobre um determinado valor e retorne uma Função Interna que execute uma outra parte do Algoritmo que trabalha com uma gama maior de valores, há um ganho de performance maior. Enquanto no primeiro cenário tanto a Função Externa quanto a Interna é chamada três vezes, os Algoritmos que trabalham com os dois Parâmetros são executados em mesma quantidade. Já no segundo cenário, a Função Externa é chamada apenas uma única vez para trabalhar com único valor e produzir o resultado parcial que é esperado para três situações diferentes, retornando em uma variável a outra parte do Algoritmo que irá operar em cima dos demais valores que fazem a diferença para a geração dos resultados discriminados.

---
+++ funcao/retornar_funcao.dart
@@ -0,0 +1,18 @@
+int Function(int) parcialSum(int numberOne) {
+  int numberThree = 1;
+  return (numberTwo) {
+    return numberOne + numberTwo + numberThree;
+  };
+}
+
+main() {
+  print('Sum: 2 + 3 = ${parcialSum(2)(3)}');
+  print('Sum: 2 + 7 = ${parcialSum(2)(7)}');
+  print('Sum: 2 + 10 = ${parcialSum(2)(10)}');
+  print('---');
+
+  var finalSum = parcialSum(2);
+  print('Sum: 2 + 3 = ${finalSum(3)}');
+  print('Sum: 2 + 7 = ${finalSum(7)}');
+  print('Sum: 2 + 10 = ${finalSum(10)}');
+}
