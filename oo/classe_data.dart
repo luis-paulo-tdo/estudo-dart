fdd664d894242f78f6b46f3aeb9706266eaf091d
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-23 05:48:33 -0300

    Demonstração básica sobre o conceito de Classes dentro do Paradigma de Orientação a Objeto. Uma Classe é definida basicamente como um molde utilizado para a criação de múltiplos Objetos, definindo para estes uma série de atributos e comportamentos que o caracterizam como a representação de um objeto ou entidade do mundo real. Ao serem criadas, geralmente recebem uma nomenclatura em TitleCase (letras iniciais maiúscula para cada palavra), e mantêm consigo uma série de variáveis e constantes cujos valores irão caracterizar cada Objeto criado. Um Objeto, por sua vez, é uma instância de uma Classe que ocupa um determinado espaço de memória. Pode-se dizer que Objeto e Instância são sinônimos, sendo entidades concretas que foram moldadas a partir de uma Classe. Para instanciar uma classe, pode ser utilizado o comando 'new' ou não, e através da Notação Ponto, é possível acessar seus atributos.

---
+++ oo/classe_data.dart
@@ -0,0 +1,20 @@
+class SimpleDate {
+  int day;
+  int month;
+  int year;
+}
+
+main() {
+  var startDate = new SimpleDate();
+  startDate.day = 23;
+  startDate.month = 3;
+  startDate.year = 2021;
+
+  SimpleDate endDate = SimpleDate();
+  endDate.day = 25;
+  endDate.month = 4;
+  endDate.year = 2022;
+
+  print('Start Date: ${startDate.day}/${startDate.month}/${startDate.year}');
+  print('End Date: ${endDate.day}/${endDate.month}/${endDate.year}');
+}
