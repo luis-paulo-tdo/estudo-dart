4b50f8f843cc65f8e5f63b9c049fe17b8f980642
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-23 07:34:22 -0300

    Demonstração do conceito, da criação e do uso de Métodos de Classe. Os Métodos nada mais são que Funções implementadas dentro de Classes, e servem para delegar a estas comportamentos próprios os quais não precisam ser desempenhados pelas funções que fazem o uso de seus Objetos. Certas tarefas que envolvem o acesso dos Atributos de um Objeto podem ser muito trabalhosos e/ou verbosos para serem implementados fora da Classe, e utilizando esta delegação de tarefas, todos os Objetos criados a partir da Classe possuirão a capacidade de realizar estas tarefas acessando os seus próprios Atributos. Assim como uma Função, os Métodos também podem receber Parâmetros e retornar valores de um determinado Tipo, além de que não precisam receber como Parâmetro os Atributos da Classe a qual pertence, uma vez que consegue acessá-los a partir dela sem problemas. No caso do Dart, todas as Classes possuem por padrão o mMtodo toString(), que pode ser chamado de forma implícita ou explícita no momento em que é interpolado com uma String. Inicialmente, o método retorna um texto informando o nome da Classe a qual o Método pertence, mas este comportamento pode ser redefinido para retornar qualquer outro valor de String. Outra coisa sobre os Métodos, é que dentro de um Método, outro Método da mesma Classe pode ser invocado para que o ojeto realize uma tarefa dentro de outra tarefa sua.

--- oo/classe_data.dart
+++ oo/classe_data.dart
@@ -2,6 +2,18 @@ class SimpleDate {
   int day;
   int month;
   int year;
+
+  printDate() {
+    print(getDate());
+  }
+
+  getDate() {
+    return '$day/$month/$year';
+  }
+
+  String toString() {
+    return getDate();
+  }
 }
 
 main() {
@@ -15,6 +27,31 @@ main() {
   endDate.month = 4;
   endDate.year = 2022;
 
+  // Acessando atributos diretamente para imprimir data completa
   print('Start Date: ${startDate.day}/${startDate.month}/${startDate.year}');
   print('End Date: ${endDate.day}/${endDate.month}/${endDate.year}');
+  print('---');
+
+  // Delegando ao objeto a impressão da data completa
+  print('Print Start Date:');
+  startDate.printDate();
+  print('Print End Date:');
+  endDate.printDate();
+  print('---');
+
+  // Obtendo a data completa a partir do objeto
+  String start = startDate.getDate();
+  String end = endDate.getDate();
+  print('Start Date: $start');
+  print('End Date: $end');
+  print('---');
+
+  // Imprimindo o retorno do método de obter data
+  print('Start Date: ${startDate.getDate()}');
+  print('End Date: ${endDate.getDate()}');
+  print('---');
+
+  // Imprimindo o objeto de forma direta
+  print('Start Date: ${startDate}');
+  print('End Date: ${endDate}');
 }
