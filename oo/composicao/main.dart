6b97a0c62e21dba97296d752a8889bed10a73042
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-03-25 07:51:58 -0300

    Inicialização da Classe Composta Shop e impressão de alguns de seus valores e de seus Objetos mais internos através da Notação Ponto. Foram impressos o CPF do Cliente da Venda, o Primeiro Produto da Lista de Itens da Venda e o Preço Total da Lista.

--- oo/composicao/main.dart
+++ oo/composicao/main.dart
@@ -1,7 +1,27 @@
 import 'cliente.dart';
+import 'produto.dart';
+import 'venda.dart';
+import 'venda_item.dart';
 
 main() {
-  // 1 Venda
-  // 1 Cliente
-  // 3 Items com Produtos
+  var shop = Shop(
+      client: Client(name: 'Lemmy', cpf: '050.553.533.67-8'),
+      items: <ShopItem>[
+        ShopItem(
+            quantity: 30,
+            product:
+                Product(code: 1, name: 'Lapis', price: 6.00, discount: 0.5)),
+        ShopItem(
+            quantity: 20,
+            product: Product(
+                code: 2, name: 'Caderno', price: 20.00, discount: 0.25)),
+        ShopItem(
+            quantity: 100,
+            product:
+                Product(code: 3, name: 'Borracha', price: 2.00, discount: 0.5))
+      ]);
+
+  print('Total Price: R\$${shop.totalPrice}');
+  print('First Product: ${shop.items[0].product.name}');
+  print('Client\'s CPF: ${shop.client.cpf}');
 }
